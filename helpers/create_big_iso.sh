1aed451bfa6202a87269599024b57c8a4e21467e
karmab <karimboumedhel@gmail.com>
2020-11-15 20:33:07 +0100

    sync helper script

---
+++ helpers/create_big_iso.sh
@@ -0,0 +1,9 @@
+API_IP="api.upi.karmalabs.com"
+MCP="worker-static"
+curl -H "Accept: application/vnd.coreos.ignition+json; version=3.1.0" -Lk https://$API_IP:22623/config/$MCP > preconfig.ign
+cp preconfig.ign config.ign
+# uncomment to inject a password for core user
+#cat preconfig.ign | jq '.passwd.users[0] += {"passwordHash": "$1$f9F1p5ap$VIFGF2QHttm6xPeGMh/YA/"}' > config.ign
+python3 create_iso_ignition.py
+[ -f rhcos-live.x86_64.iso ] || curl https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/pre-release/latest-4.6/rhcos-live.x86_64.iso > rhcos-live.x86_64.iso
+sudo podman run --privileged --rm -v /dev:/dev -v /run/udev:/run/udev -v $PWD:/data -w /data quay.io/coreos/coreos-installer:release iso ignition embed -fi iso.ign rhcos-live.x86_64.iso
