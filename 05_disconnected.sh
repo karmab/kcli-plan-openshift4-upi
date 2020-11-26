705f950109d3f549f626978a1e4307d101f46454
karmab <karimboumedhel@gmail.com>
2020-11-26 12:15:03 +0100

    properly support ci registries

--- 05_disconnected.sh
+++ 05_disconnected.sh
@@ -41,19 +41,31 @@ update-ca-trust extract
 htpasswd -bBc /opt/registry/auth/htpasswd $REGISTRY_USER $REGISTRY_PASSWORD
 podman create --name registry --net host --security-opt label=disable -v /opt/registry/data:/var/lib/registry:z -v /opt/registry/auth:/auth:z -v /opt/registry/conf/config.yml:/etc/docker/registry/config.yml -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry" -e "REGISTRY_HTTP_SECRET=ALongRandomSecretForRegistry" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -v /opt/registry/certs:/certs:z -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key {{ disconnected_registry_image }}
 podman start registry
-export UPSTREAM_REGISTRY="{{ disconnected_origin }}"
-export RELEASE_NAME='openshift-release-dev/ocp-release'
-export OCP_RELEASE="{{ tag }}-x86_64"
-export LOCAL_REGISTRY="$REGISTRY_NAME:5000"
-export PULL_SECRET="/root/openshift_pull.json"
+{% if openshift_image != None -%}
+export UPSTREAM_REGISTRY={{ openshift_image.split('/')[0] }}
+export UPSTREAM_REPOSITORY={{ openshift_image.split('/')[1] }}
+export BRANCH={{ openshift_image.split('/')[2].split(':')[0] }}
+export RELEASE_NAME=$UPSTREAM_REPOSITORY/$BRANCH
+export OCP_RELEASE={{ openshift_image.split(':')[1] }}
+export FINAL_SOURCE=$UPSTREAM_REGISTRY/$RELEASE_NAME
+{%- else -%}
+export UPSTREAM_REGISTRY={{ disconnected_origin }}
+export UPSTREAM_REPOSITORY=openshift-release-dev
+export BRANCH=ocp-release
+export RELEASE_NAME=$UPSTREAM_REPOSITORY/$BRANCH
+export OCP_RELEASE={{ tag }}-x86_64
+export FINAL_SOURCE={{ disconnected_origin }}/ocp-release
+{%- endif %}
+export LOCAL_REGISTRY=$REGISTRY_NAME:5000
+export PULL_SECRET=/root/openshift_pull.json
 KEY=$( echo -n $REGISTRY_USER:$REGISTRY_PASSWORD | base64)
 jq ".auths += {\"$REGISTRY_NAME:5000\": {\"auth\": \"$KEY\",\"email\": \"jhendrix@karmalabs.com\"}}" < $PULL_SECRET > /root/temp.json
 cat /root/temp.json | tr -d [:space:] > $PULL_SECRET
 oc adm release mirror -a $PULL_SECRET --from=${UPSTREAM_REGISTRY}/${RELEASE_NAME}:${OCP_RELEASE} --to-release-image=${LOCAL_REGISTRY}/{{ disconnected_prefix }}/release:${OCP_RELEASE} --to=${LOCAL_REGISTRY}/{{ disconnected_prefix }}
 echo "{\"auths\": {\"$REGISTRY_NAME:5000\": {\"auth\": \"$KEY\", \"email\": \"jhendrix@karmalabs.com\"}}}" > /root/temp.json
 OPENSHIFT_VERSION=$( grep cluster-openshift-apiserver-operator /var/log/cloud-init-output.log  | head -1 | awk '{print $NF}' | sed 's/-cluster-openshift-apiserver-operator//')
-echo $REGISTRY_NAME:5000/{{ disconnected_prefix }}/release:$OCP_RELEASE > /root/version.txt
 
+echo $REGISTRY_NAME:5000/{{ disconnected_prefix }}/release:$OCP_RELEASE > /root/version.txt
 cat << EOF >> /root/install-config.yaml
 imageContentSources:
 - mirrors:
@@ -61,7 +73,7 @@ imageContentSources:
   source: quay.io/openshift-release-dev/ocp-v4.0-art-dev
 - mirrors:
   - $LOCAL_REGISTRY/{{ disconnected_prefix }}
-  source: {{ disconnected_origin }}/ocp-release
+  source: $FINAL_SOURCE
 EOF
 
 echo "additionalTrustBundle: |" >> /root/install-config.yaml
