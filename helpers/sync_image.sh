478d9e02217f1840e04d043db9f5b19c127405ac
karmab <karimboumedhel@gmail.com>
2020-11-27 15:47:05 +0100

    add sync_image.sh helper script

--- helpers/sync_image.sh
+++ helpers/sync_image.sh
@@ -3,4 +3,5 @@ REVERSE_NAME=$(dig -x $IP +short | sed 's/\.[^\.]*$//')
 REGISTRY_NAME=${REVERSE_NAME:-$(hostname -f)}
 REGISTRY=$REGISTRY_NAME:5000
 PULL_SECRET="/root/openshift_pull.json"
-skopeo copy docker://$image docker://$REGISTRY/$(echo $image | sed 's@quay.io/@@') --all --authfile $PULL_SECRET
+image=$1
+skopeo copy docker://$image docker://$REGISTRY/$(echo $image | cut -d'/' -f 2- ) --all --authfile $PULL_SECRET
