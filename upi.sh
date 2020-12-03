1da19bf498a2de2d780f5e33913c721788ebccaf
karmab <karimboumedhel@gmail.com>
2020-12-03 22:33:19 +0100

    remove blacklist stuff

--- upi.sh
+++
@@ -1,7 +0,0 @@
-IP=$(kcli info vm upi-installer -f ip -v)
-POOL=$(kcli list pool | grep default | awk -F'|' '{print $3}' | xargs)
-cd $POOL
-rm -rf upi*iso
-wget http://$IP:8080/upi-bootstrap.iso
-wget http://$IP:8080/upi-master.iso
-wget http://$IP:8080/upi-worker.iso
