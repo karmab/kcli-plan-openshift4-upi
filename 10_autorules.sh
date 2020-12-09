6b496d8b96ce4797b0c515ecff4896f82fb3c605
karmab <karimboumedhel@gmail.com>
2020-12-10 00:05:53 +0100

    kubevirt operator sub

--- 10_autorules.sh
+++ 10_autorules.sh
@@ -1,4 +1,4 @@
 export REGISTRY="$(hostname -f):5000"
 sync_image.sh quay.io/karmab/autolabeller
-sync_image.sh quay.io/karmab/signer
+sync_image.sh quay.io/karmab/autosigner
 envsubst < /root/99-autorules.yaml.sample > /root/manifests/99-autorules.yaml
