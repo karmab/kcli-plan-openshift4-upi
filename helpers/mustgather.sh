c90c41b8aafc250aa88c53e1abb5ab7d7d5971d7
karmab <karimboumedhel@gmail.com>
2021-03-19 19:43:41 +0100

    correct mustgather path

--- helpers/mustgather.sh
+++ helpers/mustgather.sh
@@ -5,4 +5,4 @@ IP=$(hostname -I | cut -d' ' -f1)
 REVERSE_NAME=$(dig -x $IP +short | sed 's/\.[^\.]*$//')
 REGISTRY_NAME=${REVERSE_NAME:-$(hostname -f)}
 REGISTRY=$REGISTRY_NAME:5000
-oc adm must-gather --image=$REGISTRY/{{ disconnected_prefix }}:$(oc get clusterversion version -o jsonpath='{.status.desired.version}-must-gather') --dest-dir /tmp/mustgather
+oc adm must-gather --image=$REGISTRY/{{ disconnected_prefix }}:$(oc get clusterversion version -o jsonpath='{.status.desired.version}-x86_64-must-gather') --dest-dir /tmp/mustgather
